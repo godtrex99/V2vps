0535d8f34819ded8509aab0443abe6229f25e906
VVIBU Tamfan <114927952+godtrex99@users.noreply.github.com>
2022-10-19 18:42:42 +0700

    Add files via upload

--- xray/cekv2ray.sh
+++ xray/cekv2ray.sh
@@ -13,14 +13,6 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/senowahyu62/perizinan/main/ipvps.txt | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-exit 0
-fi
 clear
 echo -n > /tmp/other.txt
 data=( `cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2`);
